acdffa1c1c565ae53ded8533804a62d3ef49f9cd
Rayival Dede Saputra <113332359+Rayival@users.noreply.github.com>
2023-10-04 14:49:08 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,10 @@
+const login = (username, password) => {
+  console.log("----------------------")
+  console.log("        LOGIN         ")
+  console.log("----------------------")
+  console.log("Username : " + username )
+  console.log("Password : " + password )
+  console.log("----------------------")
+}
+
+login("Rayival","1223334444")
